bb307f02f29bf568f9fa60949f7c5149a5f5478b
EntrenaDev <manuel.entrena@gmail.com>
2023-08-23 19:31:09 +0200

    feat: Added list post with info BD

---
+++ src/app/components/tweet.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {
+  Avatar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+} from "@nextui-org/react";
+
+export function Tweet({
+  userName,
+  avatarUrl,
+  userFullName,
+  content,
+}: {
+  userName: string;
+  avatarUrl: string;
+  userFullName: string;
+  content: string;
+}) {
+  return (
+    <Card className="max-w-[340px]">
+      <CardHeader className="justify-between">
+        <div className="flex gap-5">
+          <Avatar isBordered radius="full" size="md" src={avatarUrl} />
+          <div className="flex flex-col gap-1 items-start justify-center">
+            <h4 className="text-small font-semibold leading-none text-default-600">
+              {userFullName}
+            </h4>
+            <h5 className="text-small tracking-tight text-default-400">
+              @{userName}
+            </h5>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody className="px-3 py-0 text-small text-default-400">
+        <p>{content}</p>
+      </CardBody>
+      <CardFooter className="gap-3">
+        <div className="flex gap-1">
+          <p className="font-semibold text-default-400 text-small">4</p>
+          <p className=" text-default-400 text-small">Following</p>
+        </div>
+        <div className="flex gap-1">
+          <p className="font-semibold text-default-400 text-small">97.1K</p>
+          <p className="text-default-400 text-small">Followers</p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { AuthButtonServer } from "./components/auth-button-server";
+import { Tweet } from "./components/tweet";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -12,12 +13,30 @@ export default async function Home() {
   if (session === null) {
     redirect("/login");
   }
-  const { data: posts } = await supabase.from("posts").select();
+  const { data: posts } = await supabase
+    .from("posts")
+    .select("*, users(name,avatar_url,user_name)");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <AuthButtonServer />
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      {posts?.map((post) => {
+        const { id, content, users } = post;
+        const {
+          name: userName,
+          avatar_url: avatarUrl,
+          user_name: userFullName,
+        } = users;
+        return (
+          <Tweet
+            key={id}
+            userName={userName}
+            avatarUrl={avatarUrl}
+            userFullName={userFullName}
+            content={content}
+          />
+        );
+      })}
     </main>
   );
 }
